07f811877e564274b47ae70b038c12dd29b9a80b
Anthony <158334705+tony64110@users.noreply.github.com>
2024-07-19 12:14:13 +0200

    corrigé

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: 'config.env' });
+// const dotenv = require('dotenv');
+// dotenv.config({ path: 'config.env' });
 const express = require('express')
 const router = require('./route/route.js')
 
